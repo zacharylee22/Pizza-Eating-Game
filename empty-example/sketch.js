9e3b4b1690d3cfba6ccd443dbbaadf1515f2ffa1
max <61802966+maximilliany@users.noreply.github.com>
2020-04-22 23:04:09 -0700

    beginning of the pizza + beginning of the table

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,11 +1,36 @@
+let distance = 50;
+
 function setup() {
   createCanvas(1400, 725);
 }
 
 function draw(){
 	background("white");
-  ellipse(700, 362, 500);
-  fill("yellow");
+  fill(227, 178, 54);
   noStroke();
+  ellipse(700, 362, 450);
 
+  fill(250, 215, 40)
+  stroke(245, 19, 2);
+  strokeWeight(8);
+  ellipse(700, 362, 390);
+
+  pepperoni(600, 400);
+  pepperoni(800, 450);
+  pepperoni(720, 200);
+  pepperoni(680, 450);
+  pepperoni(640, 280);
+
+  stroke("black")
+  strokeWeight(2);
+  noFill();
+  rect(400, 100, 600, 530);
+
+  tablecloth(10,10);
+}
+
+function pepperoni(x,y){
+  fill(245, 19, 2);
+  noStroke();
+  ellipse(x, y, 40);
 }
