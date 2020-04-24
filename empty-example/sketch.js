a275f236f085427beb8eeb46ed6d0f6ba6d22411
max <61802966+maximilliany@users.noreply.github.com>
2020-04-24 12:15:24 -0700

    put the parts of pizza into functions, created a button

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,19 +1,21 @@
 let distance = 50;
+let value = "white";
+let c;
+let button;
 
 function setup() {
-  createCanvas(1400, 725);
+createCanvas(1400, 725);
+c = color(227, 178, 54);
+button = createButton('dough');
+  button.position(19, 19);
+  button.mousePressed(crust);
 }
 
 function draw(){
 	background("white");
-  fill(227, 178, 54);
-  noStroke();
-  ellipse(700, 362, 450);
-
-  fill(250, 215, 40)
-  stroke(245, 19, 2);
-  strokeWeight(8);
-  ellipse(700, 362, 390);
+  crust();
+  sauz();
+  cheez();
 
   pepperoni(600, 400);
   pepperoni(800, 450);
@@ -26,11 +28,36 @@ function draw(){
   noFill();
   rect(400, 100, 600, 530);
 
-  tablecloth(10,10);
+
+}
+
+function mousePressed() {
+  if (value === "white") {
+    value = (c);
+  } else {
+    value = "white";
+  }
 }
 
 function pepperoni(x,y){
   fill(245, 19, 2);
   noStroke();
   ellipse(x, y, 40);
+}
+
+function crust(){
+  fill(value);
+  noStroke();
+  ellipse(700, 362, 450);
+
+}
+
+function sauz(){
+  fill(245, 19, 2);
+  ellipse(700, 362, 398);
+}
+
+function cheez(){
+  fill(250, 215, 40)
+  ellipse(700, 362, 378);
 }
