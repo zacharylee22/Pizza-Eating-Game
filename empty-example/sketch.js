bc905e7a81d1a39196c6cf36c96497bd3ba595b3
max <61802966+maximilliany@users.noreply.github.com>
2020-05-18 12:15:08 -0700

    began merging code with zach

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -10,8 +10,24 @@ let yummycheese = false;
 let topping1 = false;
 let eating = false;
 
+let mySound;
+let dots = [];
+
+let eating=false;
+
+var timerValue = 10;
+var startButton;
+
+function preload() {
+  soundFormats('mp3', 'ogg','wav');
+  mySound = loadSound('munch.wav');
+}
+
 function setup() {
   createCanvas(1400, 725);
+
+  textAlign(CENTER);
+  setInterval(timeIt, 1000);
 
   doughbutton = createButton('Dough');
   doughbutton.style('background-color', "lightblue");
@@ -64,6 +80,55 @@ function draw(){
   strokeWeight(2);
   noFill();
   rect(400, 100, 600, 530);
+
+  for (let i=0; i<dots.length; i++){
+   dots[i].drawDot();
+ }
+
+ if (timerValue >=0) {
+   text(timerValue + " SECONDS", width / 2, height / 2);
+ }
+
+}
+
+function mousePressed() {
+  if (eating == true){
+  mySound.setVolume(0.1);
+  mySound.play();
+
+
+  let d = new dot(mouseX,mouseY)
+  dots.push(d);
+  console.log(dots)
+  }
+}
+
+function timeIt() {
+  if (eating == true){
+   if (timerValue >0) {
+    timerValue--;
+    }
+ }
+}
+
+class dot {
+
+    constructor(x,y){
+      this.x = x;
+      this.y = y;
+    }
+
+    drawDot(){
+      fill("white")
+      noStroke()
+      ellipse(this.x,this.y,60,60)
+      ellipse(this.x-25,this.y-25,60,60)
+      ellipse(this.x+15,this.y+35,50,50)
+      ellipse(this.x+50,this.y+25,65,65)
+      ellipse(this.x+35,this.y,45,45)
+      ellipse(this.x+15,this.y-20,65,65)
+      ellipse(this.x+45,this.y-20,45,45)
+    }
 }
 
 function pepperoni(x,y){
